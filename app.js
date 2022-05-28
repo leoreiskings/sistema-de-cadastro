cd0c625034850aa60f077975c4b913548a576a89
leoreiskings <leo19reis@gmail.com>
2022-05-27 22:55:28 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("Rodando o Sistema de Gerenciamento de Jogos")
 console.log("Aplicação se conectando ao Banco de Dados")
 console.log("Enviando arquivo para o GitHub")
-
+console.log("Enviando para o GitHub")
