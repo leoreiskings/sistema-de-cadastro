8e1a21b3257b9b66c4aee85fe3ce090e4f42cc42
leoreiskings <leo19reis@gmail.com>
2022-05-28 10:25:14 -0300

    preparando para subir todas as alterações do diretorio

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("Rodando o Sistema de Gerenciamento de Jogos")
 console.log("Aplicação se conectando ao Banco de Dados")
 console.log("Enviando arquivo para o GitHub")
-console.log("Enviando para o GitHub")
+console.log("Mais uma linha inserida para testar o commando Push dia 28-05-2022 as 10:19")
